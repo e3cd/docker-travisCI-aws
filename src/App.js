9fc3dc9752f3d07dd2dab116132ae11fd4086923
Allan Gao <allangao95@gmail.com>
2019-05-15 16:57:15 +1000

    another change

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React
+          React with Docker-travis-aws
         </a>
       </header>
     </div>
